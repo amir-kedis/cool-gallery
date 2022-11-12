36c919513ec19bf3869d279532028dcd199975dc
amir-kedis <amir.kedis@gmail.com>
2022-11-12 11:48:38 +0200

    Feature

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
-const canvas = document.querySelector("#canvas");
+document.addEventListener('DOMContentLoaded', main);
 
-document.body.addEventListener("mousemove", (e) => {
+function main() {
+    activateBgEffect();
+    activateCursor();
+}
 
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
+function activateBgEffect() {
+    const canvas = document.querySelector("#canvas");
 
-    const xDec = mouseX / window.innerWidth,
-          yDec = mouseY / window.innerHeight;
+    document.body.addEventListener("mousemove", (e) => {
 
-    const maxX = canvas.offsetWidth = window.innerWidth;
-    const maxY = canvas.offsetHeight = window.innerHeight;
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+
+        const xDec = mouseX / window.innerWidth,
+            yDec = mouseY / window.innerHeight;
+
+        const maxX = canvas.offsetWidth = window.innerWidth;
+        const maxY = canvas.offsetHeight = window.innerHeight;
 
 
-    let translateX = maxX * xDec * -1;
-    let translateY = maxY * yDec * -1;
+        let translateX = maxX * xDec * -1;
+        let translateY = maxY * yDec * -1;
 
-    console.log({translateX, translateY});
+        /// console.log({ translateX, translateY });
 
-    canvas.animate({
-        transform: `translate(${translateX}px, ${translateY}px)`
-    }, {
-        duration: 4000,
-        fill: "forwards",
-        easing: "ease"
+        canvas.animate({
+            transform: `translate(${translateX}px, ${translateY}px)`
+        }, {
+            duration: 4000,
+            fill: "forwards",
+            easing: "ease"
+        })
+
+    });    
+}
+
+function activateCursor() {
+    const ball = document.querySelector('.cursur-ball');
+    document.body.addEventListener("mousemove", e => {
+            const mouseX = e.clientX;
+            const mouseY = e.clientY;
+            ball.animate({
+                top: mouseY + 'px',
+                left: mouseX + 'px'
+            }, {
+                duration: 1000,
+                fill: "forwards",
+                easing: "cubic-bezier(.17,.67,.44,1.33)"
+            })
     })
-
-});
+}
